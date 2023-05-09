424cc5e3077936d0c432c551f3e6d919925e9b63
yibana <lovefy.eth@gmail.com>
2023-05-09 14:56:39 +0800

    init

--- pull_deploy.sh
+++ pull_deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# 更新代码
-echo "Updating repository..."
-git reset --hard HEAD
-git pull
-
-# 编译
-echo "Building binary..."
-go build -o gin
-
 # Check if the "-kill" parameter is passed
 if [ "$1" = "-kill" ]; then
   # Kill the existing gin process if it's running
@@ -26,6 +17,16 @@ if pgrep gin >/dev/null 2>&1; then
   echo "Stopping existing gin process..."
   pkill -f gin
 fi
+
+# 更新代码
+echo "Updating repository..."
+git reset --hard HEAD
+git pull
+# 编译
+echo "Building binary..."
+go build -o gin
+
+
 
 # Set environment variable
 export MONGO_URL="mongodb://admin:3WPIki9dXShd6ZZhGXKZ@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2"
